b282828c56ddcb049c9b073377093c970957ef7f
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2019-01-26 08:42:32 +0530

    Update ms_visionapi.py
    Fixed lint of the code

--- ms_visionapi.py
+++ ms_visionapi.py
@@ -1,4 +1,3 @@
-########### Python 2.7 #############
 from time import sleep
 
 
@@ -9,29 +8,28 @@ import sys
 import json
 from os import system
 
-#To handle the SIGINT when CTRL+C is pressed
+
 def exit_gracefully(signum,frame):
     signal.signal(signal.SIGINT, original_sigint)
     sys.exit(1)
 
 
     
-# CHANGE {MS_API_KEY} BELOW WITH YOUR MICROSOFT VISION API KEY
+
 ms_api_key = "3ce370692850429d98d3bfb773bc37c2"
 
 
 headers = {
-    # Request headers
+    
     'Content-Type': 'application/octet-stream',
     'Ocp-Apim-Subscription-Key': ms_api_key,
 }
 
 params = urllib.urlencode({
-    # Request parameters
+    
     'visualFeatures': 'Description',
 })
 
-#Saves the text to the file
 def saveTextFile(text):
     try:
         print(text)
@@ -64,18 +62,13 @@ def analyze_image(data):
 
 def tag_from_data(input):
     if input is not None:
-        # Load the original image, fetched from the URL
-        # data8uint = np.fromstring(input, np.uint8)  # Convert string to an unsigned int array
-        # Get the description/tag
+      
         result = json.loads(input)
         description = result['description']['captions'][0]['text']
         print(data)
         # img = cv2.cvtColor(cv2.imdecode(data8uint, cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB)
 
-        # columns = defaultdict(list)
-
-        # Get image captipn and keywords
-        # Concatenate them to form a single string
+       
         awsstring = "I think it is "
         awsstring += description
         awsstring += ". And the keywords are  "
@@ -95,5 +88,4 @@ if __name__ == '__main__':
     data = analyze_image(img)
     text = tag_from_data(data)
     saveTextFile(text)
-#os.system("python aws_dynamodb.py")
-####################################
+
